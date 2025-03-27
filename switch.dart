f63e3e10aebfd6cd74b413721875b0a1c8df5018
Sadik Saroar <sadiksaroar275@gmail.com>
2025-03-27 21:33:52 +0600

    Initial commit

---
+++ switch.dart
@@ -0,0 +1,17 @@
+void main() {
+  String fruit = "Mango";
+
+  switch (fruit) {
+    case "Apple":
+      print("This is an Apple.");
+      break;
+    case "Banana":
+      print("This is a Banana.");
+      break;
+    case "Mango":
+      print("This is a Mango.");
+      break;
+    default:
+      print("Unknown fruit.");
+  }
+}
